2ba665b2c618997f23013e3b70c1f42934cb6b72
Klaudia Sławińska <klaudia.slawinska94@gmail.com>
2019-09-15 20:46:48 +0200

    Add Search component

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,21 @@
+// selectors
+export const countAllCards = ({cards}) => cards.length;
+export const countVisibleCards = ({ cards, searchString }) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+export const getSearchString = ({ searchString }) => searchString;
+// action name creator
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
+// actions types
+export const SEARCH = createActionName('SEARCH');
+// action creators
+export const createAction_SEARCH = payload => ({ payload, type: SEARCH });
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    case SEARCH:
+      return   action.payload ;
+    default:
+      return statePart;
+  }
+}
+
